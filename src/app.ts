445be254429fdced9fcb33be32de0ab52f773a6e
Alexandre <eztalenda@gmail.com>
2024-08-18 00:07:41 -0300

    Fix: Ajusta constante dbURI para representar sempre uma string.

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,20 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/users'
+
 dotenv.config()
 const app = express()
 
 app.use(express.json())
 
-const dbURI = process.env.MONGO_URI
+const dbURI = process.env.MONGO_URI as string
 
-if (!dbURI) {
-  throw new Error('MONGO_URI environment variable is not defined')
-}
+app.use('/api/users', userRoutes)
 
 mongoose
   .connect(dbURI)
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error: ' + err))
+  .catch((err) => console.log('MongoDB connection error:', err))
 
 export default app
