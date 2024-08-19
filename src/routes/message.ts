c8d896e021e95e12757aae6ada4c4c20695f5a38
Alexandre <eztalenda@gmail.com>
2024-08-19 04:51:05 -0300

    fix: Ajusta rotas do messages.

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,12 +1,16 @@
 import express from 'express'
-import { getMessages, createMessage, getMessagesBetweenUsers } from '../controllers/messageController'
+import {
+  getMessages,
+  createMessage,
+  getMessagesBetweenUsers,
+  getUserConversations,
+} from '../controllers/messageController'
 
 const router = express.Router()
 
-router.get('/', getMessages)
-router.post('/', createMessage)
-
+router.get('/conversations/:userId', getUserConversations)
 router.get('/:senderId/:receiverId', getMessagesBetweenUsers)
+router.get('/', getMessages)
 router.post('/', createMessage)
 
 export default router
